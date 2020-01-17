651ddbba7d4392d33d61df7c4f7a40c385d7557c
Alfonso Miguel Evangelista Concepción <a46.evangelista@gmail.com>
2020-01-16 22:02:44 -0400

    get: transactions api method

--- src/rutas/transaction.php
+++ src/rutas/transaction.php
@@ -3,16 +3,21 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 // GET: Todos los clientes.
-$app->get('/api/customers', function(Request $request, Response $response){
+$app->get('/api/transactions/{id}', function(Request $request, Response $response){
     try{
         $db  = new db();
         $db = $db->conectDB();
+        $user_id = $request->getAttribute('id');
+        
+        $sql = "select id, description, amount, type, user_id, date_format(date, '%Y-%m-%dT%TZ') as date ".
+               "from transaction where user_id = " . $user_id;
 
-        $resultado = $db->query('SELECT cu.*, dt.* FROM customer as cu join doc_type as dt on cu.document_type = dt.id');
-        if($resultado ->rowCount()>0)
+        $resultado = $db->query($sql);
+
+        if($resultado ->rowCount() > 0)
         {
             $resultado = $resultado->fetchAll(PDO::FETCH_OBJ);
-        }else $resultado = array('message'=>'No existen clientes en la base de datos');
+        }else $resultado = array('message' => 'No existen transacciones para este usuario');
 
         return jsonResponse($response, $resultado);
       
